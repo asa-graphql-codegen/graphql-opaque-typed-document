deaf41c4d63371fff8b95713ad2f1bfd272bc0a3
Asa Kusuma <akusuma@linkedin.com>
2022-03-14 15:22:38 -0700

    Initial types

---
+++ index.ts
@@ -0,0 +1,12 @@
+declare const Result: unique symbol;
+declare const QueryValues: unique symbol;
+
+declare class OpaqueTypedDocument<
+  R extends Record<string, unknown>,
+  V extends Record<string, unknown>
+> {
+  private [Result]: R;
+  private [QueryValues]: V;
+}
+
+export { type OpaqueTypedDocument };
