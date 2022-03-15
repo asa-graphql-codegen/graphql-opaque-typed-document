02debd92cfa4bbc8dcc5ef62b2d1bab51acd60af
Asa Kusuma <akusuma@linkedin.com>
2022-03-14 21:52:15 -0700

    Release 0.1.0

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ declare const QueryValues: unique symbol;
 
 declare class OpaqueTypedDocument<
   R extends Record<string, unknown>,
-  V extends Record<string, unknown>
+  V extends Record<string, unknown> = Record<string, unknown>
 > {
   private [Result]: R;
   private [QueryValues]: V;
